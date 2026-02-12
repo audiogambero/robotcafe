dac2cc96ef3b0a391c3c56819ca94aba346920d7
Samuele Gamberoni <samuele.gamberoni.audio@gmail.com>
2026-02-12 09:02:39 +0100

    load cafe map

--- src/title.c
+++ src/title.c
@@ -3,15 +3,15 @@
 #include "title.h"
 #include "utils.h"
 
-#include "../res/dungeon_tiles.h"
-#include "../res/dungeon_map.h"
+#include "../res/cafe_map.h"
+#include "../res/cafe_tiles.h"
 
 void title_setup_gfx()
 {
     SCREEN_WHITE;
     DISPLAY_OFF;
-    set_bkg_data(0, 79u, dungeon_tiles);
-    set_bkg_tiles(0, 0, 32u, 32u, dungeon_mapPLN0);
+    set_bkg_data(0, 127u, cafe_tiles);
+    set_bkg_tiles(0, 0, 20u, 18u, cafe_mapPLN0);
     SHOW_BKG;
     DISPLAY_ON;
 }
